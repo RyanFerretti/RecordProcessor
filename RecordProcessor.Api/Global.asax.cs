15fdc3205272e41abddb549b2c7bafe57443451d
Ryan Ferretti <ryan.ferretti@rallyhealth.com>
2015-08-11 14:13:58 -0500

    added new test to exercise the api's container setup

--- RecordProcessor.Api/Global.asax.cs
+++ RecordProcessor.Api/Global.asax.cs
@@ -1,20 +1,15 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Web;
 using System.Web.Http;
 using Autofac;
 using Autofac.Integration.WebApi;
 using RecordProcessor.Api.IoC;
-using RecordProcessor.Application;
-using RecordProcessor.Application.Domain;
-using RecordProcessor.Application.Repositories;
 
 namespace RecordProcessor.Api
 {
     public class WebApiApplication : HttpApplication
     {
-        private readonly string[] _recordPaths =
+        public static readonly string[] RecordPaths =
         {
             "C:\\Users\\Ryan Ferretti\\Documents\\visual studio 2013\\Projects\\RecordProcessor\\records_comma.txt",
             "C:\\Users\\Ryan Ferretti\\Documents\\visual studio 2013\\Projects\\RecordProcessor\\records_pipe.txt",
@@ -25,15 +20,13 @@ namespace RecordProcessor.Api
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
             var container = BuildContainer();
-            SetupRecordsAndInMemoryDataStore(container);
+            SetupPostInitDependencies(container);
         }
 
-        protected void SetupRecordsAndInMemoryDataStore(IContainer container)
+        protected void SetupPostInitDependencies(IContainer container)
         {
-            var builder = container.Resolve<IBuilder<Record>>();
-            var results = builder.Build(_recordPaths, "0");
             var newBuilder = new ContainerBuilder();
-            newBuilder.Register(c => new InMemoryRecordRepository(results.ToList())).As<IRecordRepository>().SingleInstance();
+            newBuilder.RegisterModule(new PostApplyApiModule(container,RecordPaths));
             newBuilder.Update(container);
         }
 
